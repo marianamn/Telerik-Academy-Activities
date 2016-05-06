7e34830c18e8089aa2f44c379a3273860bb3cd9c
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-06 19:13:01 +0300

    NFactorial task - done

--- Homeworks/04. Methods/NFactorial/NFactorial.cs
+++ Homeworks/04. Methods/NFactorial/NFactorial.cs
@@ -1,33 +1,36 @@
-﻿/*Problem 10. N Factorial
-• Write a program to calculate  n!  for each  n  in the range [ 1..100 ].
-
-Hint: Implement first a method that multiplies a number represented as array of digits 
-by given integer number.   */
-
-using System;
+﻿using System;
 using System.Numerics;
+
 class NFactorial
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+
         BigInteger[] array = new BigInteger[101];
 
         array = NFactorialMethod(array);
         for (int i = 1; i < array.Length; i++)
         {
-            Console.WriteLine("{0}!={1}",i, array[i]);
+            if (i == n)
+            {
+                Console.WriteLine(array[i]);
+                break;
+            }
         }
-        Console.WriteLine();
     }
 
     static BigInteger[] NFactorialMethod(BigInteger[] array)
     {
         BigInteger product = 1;
-        for (int i = 1; i <array.Length; i++)
+
+        for (int i = 1; i < array.Length; i++)
         {
             product *= i;
-            array [i] = product;
+            array[i] = product;
         }
+
         return array;
     }
 }
+
