4757c030e9fdd726db52e8c767e9d4527f0787fe
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-06 16:20:25 +0300

    SayHello task - done

--- Homeworks/04. Methods/SayHello/SayHello.cs
+++ Homeworks/04. Methods/SayHello/SayHello.cs
@@ -1,40 +1,15 @@
-﻿/*Problem 1. Say Hello
-• Write a method that asks the user for his name and prints  “Hello, <name>”  
-• Write a program to test this method.
+﻿using System;
 
-Example:
-input                output
-Peter                Hello, Peter! */
-
-using System;
-using System.Linq;
 class SayHello
 {
     static void Main()
     {
-        string name = AskName();
-        CheckName(name);
-    }
-
-    static string AskName()
-    {
-        Console.WriteLine("Enter your name:");
         string name = Console.ReadLine();
-        return name;
+        PrintName(name);
     }
-
-    //check if the string consist of letters
-    static void CheckName(string input)
+    
+    static void PrintName(string name)
     {
-        bool isLetter = input.All(char.IsLetter);
-        if (isLetter)
-        {
-            Console.WriteLine("Hello, {0}!", input);
-        }
-        else
-        {
-            Console.WriteLine("Invalid input!");
-        }
+        Console.WriteLine("Hello, {0}!", name);
     }
-
 }
