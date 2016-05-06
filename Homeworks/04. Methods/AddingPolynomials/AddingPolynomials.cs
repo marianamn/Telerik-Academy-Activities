c5c0f716ed1a671b4a754a6e7b6559d7f08ecd74
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-06 19:52:03 +0300

    AddingPolynomials task - done

--- Homeworks/04. Methods/AddingPolynomials/AddingPolynomials.cs
+++ Homeworks/04. Methods/AddingPolynomials/AddingPolynomials.cs
@@ -1,102 +1,39 @@
-﻿/*Problem 11. Adding polynomials
-• Write a method that adds two polynomials.
-• Represent them as arrays of their coefficients.
+﻿using System;
 
-Example:
-x^2 + 5 =  1 x^2 +  0 x +  5  => { 5 ,  0 ,  1 }*/
-
-
-
-using System;
 class AddingPolynomials
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter first polynomial degree:");
         int n = int.Parse(Console.ReadLine());
-        int[] polynomial1 = new int [n+1];
-        PolynomialConsoleInput(n + 1, polynomial1);
-        Console.WriteLine("First polynomial: {0}",PrintPolynomial(n, polynomial1));
-        Console.WriteLine();
+        string[] input1 = Console.ReadLine().Split(' ');
+        string[] input2 = Console.ReadLine().Split(' ');
 
-        Console.Write("Enter second polynomial degree:");
-        int m = int.Parse(Console.ReadLine());
-        int[] polynomial2 = new int[m+1];
-        PolynomialConsoleInput(m + 1, polynomial2);
-        Console.WriteLine("Second polynomial: {0}", PrintPolynomial(m, polynomial2));
-        Console.WriteLine();
+        int[] firstPolynomial = new int [n];
 
-        Console.WriteLine("Sum of the two polynomials is:");
-        SumOfTwoPolynomials(polynomial1, polynomial2);
+        for (int i = 0; i < n; i++)
+        {
+            firstPolynomial[i] = int.Parse(input1[i]);
+        }
 
+        int[] secondPolynomial = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            secondPolynomial[i] = int.Parse(input2[i]);
+        }
+
+        SumOfTwoPolynomials(firstPolynomial, secondPolynomial, n);
     }
 
-    
-
-    public static void PolynomialConsoleInput(int n, int[] array)
+    static void SumOfTwoPolynomials(int[] firstPolynomial, int[] secondPolynomial, int n)
     {
-        for (int i = n - 1; i >= 0; i--)
+        int[] sums = new int[n];
+
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("x^{0}=", i);
-            array[i] = int.Parse(Console.ReadLine());
+           sums[i] = firstPolynomial[i] + secondPolynomial[i];
         }
-        Console.WriteLine();
+
+        Console.WriteLine(string.Join(" ", sums));
     }
-
-    static string PrintPolynomial(int n, int[] array)
-    {
-        string polynomial = "";
-        for (int i = n; i >= 0; i--)
-        {
-            if (i == n)
-            {
-                polynomial = polynomial + array[i] + "*" + "x^" + i;
-            }
-            else
-            {
-                polynomial = polynomial + "+" + array[i] + "*" + "x^" + i;
-            }
-
-        }
-        return polynomial;
-    }
-
-    static void SumOfTwoPolynomials(int[] polynomial1, int[] polynomial2)
-    {
-        int[] sum = new int[Math.Max(polynomial1.Length, polynomial2.Length)];
-        for (int i = 0; i < sum.Length; i++)
-        {
-            if (i < Math.Min(polynomial1.Length, polynomial2.Length))
-            {
-                sum[i] = polynomial1[i] + polynomial2[i];
-            }
-            else
-            {
-                if (polynomial1.Length > polynomial2.Length) 
-                 {
-                     sum[i] = polynomial1[i]; 
-                 } 
-                 else 
-                 {
-                     sum[i] = polynomial2[i]; 
-                 } 
-            }
-        }
-
-
-        for (int i = 0; i <= sum.Length-1; i++)
-        {
-            if (i == (sum.Length - 1))
-            {
-                Console.Write(sum[i]);
-            }
-            else
-            {
-                Console.Write("{0}, ", sum[i]);
-            }
-        }
-        Console.WriteLine();
-    }
-
-    
 }
