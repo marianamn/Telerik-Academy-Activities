05a08f0526c3ac39303e2d5f19fe76c287c112a9
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-06 16:45:18 +0300

    AppearanceCount problem - done

--- Homeworks/04. Methods/AppearanceCount/AppearanceCount.cs
+++ Homeworks/04. Methods/AppearanceCount/AppearanceCount.cs
@@ -1,46 +1,28 @@
-﻿/*Problem 4. Appearance count
-• Write a method that counts how many times given number appears in given array.
-• Write a test program to check if the method is workings correctly.  */
+﻿using System;
 
-
-/*Examples for check 
-input                              check number           result
-2, 1, 1, 2, 3, 3, 2, 2, 2, 1            2                   5       */
-
-using System;
-using System.Linq;
 class AppearanceCount
 {
     static void Main()
     {
-        Console.WriteLine("Enter number sequence, separated by commas:");
-        string input = Console.ReadLine();
-        string[] numbers = input.Split(new char[] { ',', ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+        int n = int.Parse(Console.ReadLine());
+        string[] input = Console.ReadLine().Split(' ');
+        int[] numbers = new int[n];
 
-        //transferring string numbers into int
-        int[] array = new int[numbers.Length];
-        for (int i = 0; i < numbers.Length; i++)
+        for (int i = 0; i < n; i++)
         {
-            array[i] = int.Parse(numbers[i]);
+            numbers[i] = int.Parse(input[i]);
         }
-
-        Console.WriteLine("Enter number to check:");
+        
         int number = int.Parse(Console.ReadLine());
-        
+        int count = CountNumberAppearance(number, numbers);
 
-        
-        if (CheckNumberCountExist(number, array) == true)
-        {
-            CountNumber(number, array);
-        }
-  
-
+        Console.WriteLine(count);
     }
 
-    static void CountNumber(int num, int[] array)
+    static int CountNumberAppearance(int num, int[] array)
     {
-        Array.Sort(array);
         int count = 0;
+
         for (int i = 0; i < array.Length; i++)
         {
             if (array[i] == num)
@@ -52,32 +34,7 @@ class AppearanceCount
                 continue;
             }
         }
-        Console.WriteLine("Number {0} appears {1} times.", num, count);
+
+        return count;
     }
-        
-        
-        static bool CheckNumberCountExist(int num, int [] array)
-        {
-            int isNumberExist = 0;
-            int sum = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i]==num)
-                {
-                    isNumberExist ++;
-                    sum++;
-                }
-            }
-
-
-            if (sum == 0)
-            {
-                Console.WriteLine("Invalid input! Number you have entered does not exist in array.");
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
 }
