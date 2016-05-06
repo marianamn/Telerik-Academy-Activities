459e33fcf09ff4c543b1bbdc673c4ace1764b839
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-06 20:02:39 +0300

    SubstractingPolynomials task - done README added

--- Homeworks/04. Methods/SubtractingPolynomials/SubtractingPolynomials.cs
+++ Homeworks/04. Methods/SubtractingPolynomials/SubtractingPolynomials.cs
@@ -1,135 +1,66 @@
-﻿/*Problem 12. Subtracting polynomials
- • Extend the previous program to support also subtraction and multiplication of polynomials.*/
+﻿using System;
 
-using System;
-class SubtractingPolynomials
+class SubstractingPolynomials
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter first polynomial degree:");
         int n = int.Parse(Console.ReadLine());
-        int[] polynomial1 = new int[n + 1];
-        PolynomialConsoleInput(n + 1, polynomial1);
-        Console.WriteLine("First polynomial: {0}", PrintPolynomial(n, 0, polynomial1));
-        Console.WriteLine();
+        string[] input1 = Console.ReadLine().Split(' ');
+        string[] input2 = Console.ReadLine().Split(' ');
 
-        Console.Write("Enter second polynomial degree:");
-        int m = int.Parse(Console.ReadLine());
-        int[] polynomial2 = new int[m + 1];
-        PolynomialConsoleInput(m + 1, polynomial2);
-        Console.WriteLine("Second polynomial: {0}", PrintPolynomial(m, 0, polynomial2));
-        Console.WriteLine();
+        int[] firstPolynomial = new int[n];
 
+        for (int i = 0; i < n; i++)
+        {
+            firstPolynomial[i] = int.Parse(input1[i]);
+        }
 
-        //product of two polynomials
-        Console.WriteLine("Product of the two polynomials is:");
-        ProductOfTwoPolynomials(polynomial1, polynomial2);
+        int[] secondPolynomial = new int[n];
 
-        //substraction of two polynomials
-        Console.WriteLine("Substraction of the two polynomials is:");
-        SubstractionOfTwoPolynomials(polynomial1, polynomial2);
-        
+        for (int i = 0; i < n; i++)
+        {
+            secondPolynomial[i] = int.Parse(input2[i]);
+        }
+
+        SumOfTwoPolynomials(firstPolynomial, secondPolynomial, n);
+        SubstractOfTwoPolynomials(firstPolynomial, secondPolynomial, n);
+        MultiplicationOfTwoPolynomials(firstPolynomial, secondPolynomial, n);
     }
 
-    public static void PolynomialConsoleInput(int n, int[] array)
+    static void SumOfTwoPolynomials(int[] firstPolynomial, int[] secondPolynomial, int n)
     {
-        for (int i = n - 1; i >= 0; i--)
+        int[] sums = new int[n];
+
+        for (int i = 0; i < n; i++)
         {
-            Console.Write("x^{0}=", i);
-            array[i] = int.Parse(Console.ReadLine());
+            sums[i] = firstPolynomial[i] + secondPolynomial[i];
         }
+
+        Console.WriteLine(string.Join(" ", sums));
     }
 
-    static string PrintPolynomial(int end, int start, int[] array)
+    private static void SubstractOfTwoPolynomials(int[] firstPolynomial, int[] secondPolynomial, int n)
     {
-        string polynomial = "";
-        for (int i = end; i >= start; i--)
-        {
-            if (i == end)
-            {
-                polynomial = polynomial + array[i] + "*" + "x^" + i;
-            }
-            else
-            {
-                polynomial = polynomial + "+" + array[i] + "*" + "x^" + i;
-            }
+        int[] substracts = new int[n];
 
+        for (int i = 0; i < n; i++)
+        {
+            substracts[i] = firstPolynomial[i] - secondPolynomial[i];
         }
-        return polynomial;
+
+        Console.WriteLine(string.Join(" ", substracts));
     }
 
-
-    static void ProductOfTwoPolynomials(int[] polynomial1, int[] polynomial2)
+    private static void MultiplicationOfTwoPolynomials(int[] firstPolynomial, int[] secondPolynomial, int n)
     {
-        int[] product = new int[Math.Max(polynomial1.Length, polynomial2.Length)];
-        for (int i = 0; i < product.Length; i++)
+        int[] multiplication = new int[n];
+
+        for (int i = 0; i < n; i++)
         {
-            if (i < Math.Min(polynomial1.Length, polynomial2.Length))
-            {
-                product[i] = polynomial1[i] * polynomial2[i];
-            }
-            else
-            {
-                if (polynomial1.Length > polynomial2.Length)
-                {
-                    product[i] = polynomial1[i];
-                }
-                else
-                {
-                    product[i] = polynomial2[i];
-                }
-            }
+            multiplication[i] = firstPolynomial[i] - secondPolynomial[i];
         }
 
-
-        for (int i = 0; i <= product.Length - 1; i++)
-        {
-            if (i == (product.Length - 1))
-            {
-                Console.Write(product[i]);
-            }
-            else
-            {
-                Console.Write("{0}, ", product[i]);
-            }
-        }
-        Console.WriteLine();
-    }
-
-    static void SubstractionOfTwoPolynomials(int[] polynomial1, int[] polynomial2)
-    {
-        int[] product = new int[Math.Max(polynomial1.Length, polynomial2.Length)];
-        for (int i = 0; i < product.Length; i++)
-        {
-            if (i < Math.Min(polynomial1.Length, polynomial2.Length))
-            {
-                product[i] = polynomial1[i] - polynomial2[i];
-            }
-            else
-            {
-                if (polynomial1.Length > polynomial2.Length)
-                {
-                    product[i] = polynomial1[i];
-                }
-                else
-                {
-                    product[i] = polynomial2[i];
-                }
-            }
-        }
-
-
-        for (int i = 0; i <= product.Length - 1; i++)
-        {
-            if (i == (product.Length - 1))
-            {
-                Console.Write(product[i]);
-            }
-            else
-            {
-                Console.Write("{0}, ", product[i]);
-            }
-        }
-        Console.WriteLine();
+        Console.WriteLine(string.Join(" ", multiplication));
     }
 }
+
