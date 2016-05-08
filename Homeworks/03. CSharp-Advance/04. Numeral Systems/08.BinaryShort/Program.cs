9b319835d73440228a6550d9ec50603ec2db39c3
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-08 16:43:25 +0300

    BinaryShort task - done

--- Homeworks/03. CSharp-Advance/04. Numeral Systems/08.BinaryShort/Program.cs
+++ Homeworks/03. CSharp-Advance/04. Numeral Systems/08.BinaryShort/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _08.BinaryShort
+﻿namespace _08.BinaryShort
 {
-    class Program
+    using System;
+
+    public class BinaryShort
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            short input = short.Parse(Console.ReadLine());
+
+            string inputToBinary = ShortToBinary(input);
+
+            // the first bit is the sign (0 for positive, 1 for negative), the next 15 bits are for the number  
+            Console.WriteLine(inputToBinary.Substring(1, 15));
+        }
+
+        public static string ShortToBinary(short number)
+        {
+            // get the number as an array of 4 bytes 
+            byte[] bytesInput = BitConverter.GetBytes(number); 
+            string inputToBinary = string.Empty;
+
+            // convert each byte to its binary representation and pad with zeroes to get 8 bits for each byte 
+            for (int i = 0; i < bytesInput.Length; i++)
+            {                                       
+                inputToBinary = Convert.ToString(bytesInput[i], 2).PadLeft(8, '0') + inputToBinary;
+            }
+
+            return inputToBinary;
         }
     }
 }
