b3fa00d5ea1dc89b9ec32b6a0558ca834d077029
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-08 12:17:50 +0300

    Fixed Largest area in matrix task. Sequence in matrix - still not fully fulfilled - TODO find bug

--- Homeworks/03. CSharp-Advance/02. Multidimensional Arrays/LargestAreaInMatrix/LargestAreaInMatrix.cs
+++ Homeworks/03. CSharp-Advance/02. Multidimensional Arrays/LargestAreaInMatrix/LargestAreaInMatrix.cs
@@ -1,81 +1,176 @@
-﻿/*Problem 7.* Largest area in matrix
- • Write a program that finds the largest area of equal neighbour elements in a rectangular matrix 
-   and prints its size.
+﻿using System;
+using System.Linq;
 
-Example:
-matrix             result
-1 3 2 2 2 4          13
-3 3 3 2 4 4 
-4 3 1 2 3 3 
-4 3 1 3 3 1 
-4 3 3 3 1 1 
- 
-Hint: you can use the algorithm Depth-first search or Breadth-first search.*/
-
-using System;
 class LargestAreaInMatrix
 {
     static void Main()
     {
-        string[] sizes = Console.ReadLine().Split(' ');
-        int n = int.Parse(sizes[0]);
-        int m = int.Parse(sizes[1]);
+        string text = Console.ReadLine();
+        var result = text
+            .Split(' ')
+            .Select(z => Convert.ToInt16(z))
+            .ToArray();
+        short lines = result[0];
+        short members = result[1];
+        var matrix = new short?[lines][];
 
-        int[,] matrix = new int[n, m];
-        bool[,] visited = new bool[n, m];
-
-        for (int row = 0; row < n; row++)
+        for (short i = 0; i < lines; i++)
         {
-            string[] rowsNum = Console.ReadLine().Split(' ');
+            matrix[i] = new short?[members];
+        }
 
-            for (int col = 0; col < m; col++)
+        for (short i = 0; i < lines; i++)
+        {
+            text = null;
+            text = Console.ReadLine();
+            result = text
+            .Split(' ')
+            .Select(z => Convert.ToInt16(z))
+            .ToArray();
+            text = null;
+            for (short j = 0; j < members; j++)
             {
-                matrix[row, col] = int.Parse(rowsNum[col]);
+                matrix[i][j] = result[j];
             }
         }
 
-        PrintResult(matrix, visited);
-    }
+        short max = 1;
+        short counter = 0;
+        short? curent;
 
-    //Depth-first search method
-    static int DepthFirstSearch(int[,] matrix, bool[,] visited, int row, int col, int value)
-    {
-        if (row < 0 || col < 0 || row >= matrix.GetLength(0) || col >= matrix.GetLength(1))
+        for (short i = 0; i < lines; i++)
         {
-            return 0;
-        }
-
-        if (visited[row, col])
-        {
-            return 0;
-        }
-
-        if (matrix[row, col] != value)
-        {
-            return 0;
-        }
-
-        visited[row, col] = true;
-
-        return DepthFirstSearch(matrix, visited, row, col + 1, value) +
-               DepthFirstSearch(matrix, visited, row, col - 1, value) +
-               DepthFirstSearch(matrix, visited, row + 1, col, value) +
-               DepthFirstSearch(matrix, visited, row - 1, col, value) + 1;
-    }
-
-    static void PrintResult(int[,] matrix, bool[,] visited)
-    {
-        int result = 0;
-
-        for (int row = 0; row < matrix.GetLength(0); row++)
-        {
-            for (int col = 0; col < matrix.GetLength(1); col++)
+            for (short j = 0; j < members; j++)
             {
-                result = Math.Max(result, DepthFirstSearch(matrix, visited, row, col, matrix[row, col]));
+                counter = 0;
+                if (matrix[i][j] == null) //number is part of checked sequence
+                {
+                    continue;
+                }
+                else
+                {
+                    //unckeched number
+                    counter = 1;
+                    curent = matrix[i][j];
+                    matrix[i][j] = null;
+
+                    //with null mark all numbers in curent sequence
+                    counter = Finder(matrix, i, j, ref curent, ref counter);
+                    if (counter > max)
+                    {
+                        max = counter;
+                    }
+                }
+
             }
         }
 
-        Console.WriteLine(result);
+        Console.WriteLine(max);
+    }
+
+    static short Finder(short?[][] matrix, short row, short col, ref short? curent, ref short counter)
+    {
+        if (col > 0 && matrix[row][col - 1] == curent) //left
+        {
+            counter++;
+            matrix[row][col - 1] = null;
+            counter = Finder(matrix, row, (short)(col - 1), ref curent, ref counter);
+        }
+
+        if (row < matrix.Length - 1 && matrix[row + 1][col] == curent) //down
+        {
+            counter++;
+            matrix[row + 1][col] = null;
+            counter = Finder(matrix, (short)(row + 1), col, ref curent, ref counter);
+        }
+
+        if (col < matrix[row].Length - 1 && matrix[row][col + 1] == curent) //right
+        {
+            counter++;
+            matrix[row][col + 1] = null;
+            counter = Finder(matrix, row, (short)(col + 1), ref curent, ref counter);
+        }
+
+        if (row > 0 && matrix[row - 1][col] == curent) //up
+        {
+            counter++;
+            matrix[row - 1][col] = null;
+            counter = Finder(matrix, (short)(row - 1), col, ref curent, ref counter);
+        }
+
+        return counter;
     }
 }
+
+//second solution - time limit??
+//using System;
+//class LargestAreaInMatrix
+//{
+//    static void Main()
+//    {
+//        string[] sizes = Console.ReadLine().Split(' ');
+//        short n = short.Parse(sizes[0]);
+//        short m = short.Parse(sizes[1]);
+//
+//        short[,] matrix = new short[n, m];
+//        bool[,] visited = new bool[n, m];
+//
+//        for (short row = 0; row < n; row++)
+//        {
+//            string[] rowsNum = Console.ReadLine().Split(' ');
+//
+//            for (short col = 0; col < m; col++)
+//            {
+//                matrix[row, col] = short.Parse(rowsNum[col]);
+//            }
+//        }
+//
+//        PrintResult(matrix, visited);
+//    }
+//
+//    //Depth-first search method
+//    static short DepthFirstSearch(short[,] matrix, bool[,] visited, int row, int col, short value)
+//    {
+//        short result = 0;
+//
+//        if (row < 0 || col < 0 || row >= matrix.GetLength(0) || col >= matrix.GetLength(1))
+//        {
+//            return 0;
+//        }
+//
+//        if (visited[row, col])
+//        {
+//            return 0;
+//        }
+//
+//        if (matrix[row, col] != value)
+//        {
+//            return 0;
+//        }
+//
+//        visited[row, col] = true;
+//
+//        result = (short)(DepthFirstSearch(matrix, visited, row, col + 1, value) +
+//                         DepthFirstSearch(matrix, visited, row, col - 1, value) +
+//                         DepthFirstSearch(matrix, visited, row + 1, col, value) +
+//                         DepthFirstSearch(matrix, visited, row - 1, col, value) + 1);
+//
+//        return result;
+//    }
+//
+//    static void PrintResult(short[,] matrix, bool[,] visited)
+//    {
+//        short result = 0;
+//
+//        for (short row = 0; row < matrix.GetLength(0); row++)
+//        {
+//            for (short col = 0; col < matrix.GetLength(1); col++)
+//            {
+//                result = Math.Max(result, DepthFirstSearch(matrix, visited, row, col, matrix[row, col]));
+//            }
+//        }
+//
+//        Console.WriteLine(result);
+//    }
+//}
 
--- Homeworks/03. CSharp-Advance/02. Multidimensional Arrays/SequenceNMatrix/SequenceNMatrix.cs
+++ Homeworks/03. CSharp-Advance/02. Multidimensional Arrays/SequenceNMatrix/SequenceNMatrix.cs
@@ -1,24 +1,7 @@
-﻿/*Problem 3. Sequence n matrix
-• We are given a matrix of strings of size  N x M . 
-  Sequences in the matrix we define as sets of several neighbour elements located on the same line, 
-  column or diagonal.
-• Write a program that finds the longest sequence of equal strings in the matrix.
-
-Example:
-matrix                 result                matrix             result
-N=3; M=4                                     N=3; M=3
-ha fifi ho hi          ha, ha, ha            s qq s             s, s, s
-fo ha hi xx                                  pp pp s 
-xxx ho ha xx                                 pp qq s                            */
-
-using System;
-using System.Linq;
+﻿using System;
 
 class SequenceNMatrix
 {
-    static string bestSequence = String.Empty;
-    static int bestCount = 0;
-
     static void Main()
     {
         string[] sizes = Console.ReadLine().Split(' ');
@@ -37,77 +20,96 @@ class SequenceNMatrix
             }
         }
 
-        for (int row = 0; row < matrix.GetLength(0); row++)
+        int count = 1;
+        int maxCount = 1;
+
+        //check if there are equal elements in rows
+        for (int row = 0; row < n; row++)
         {
-            for (int col = 0; col < matrix.GetLength(1); col++)
+            for (int col = 0; col < m - 1; col++)
             {
-                CheckForSequence(matrix, row, col);
+                if (matrix[row, col] == matrix[row, col + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                }
+            }
+
+            count = 1;
+        }
+
+        //check if there are equal elements in columns
+        for (int col = 0; col < m; col++)
+        {
+            for (int row = 0; row < n - 1; row++)
+            {
+                if (matrix[row, col] == matrix[row + 1, col])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+        
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                }
+            }
+        
+            count = 1;
+        }
+
+        //check if there are equal elements in left to right diagonal
+        for (int row = 0, col = 0; row < n - 1 && col < m - 1; row++, col++)
+        {
+            if ((matrix[row, col] == matrix[row + 1, col + 1]))
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+            }
+        
+            if (count > maxCount)
+            {
+                maxCount = count;
             }
         }
+        count = 1;
 
-        Console.WriteLine(bestCount);
-    }
 
-    private static void CheckForSequence(string[,] matrix, int row, int col)
-    {
-        int diagonal = CheckDiagonal(matrix, row, col);
-        int right = CheckRight(matrix, row, col);
-        int down = CheckDown(matrix, row, col);
-
-        if (diagonal > bestCount ||
-            right > bestCount ||
-            down > bestCount)
+        //check if there are equal elements in diagonal - right to left 
+        for (int row = 0, col = 0; row < n - 1 && col > 0; row++, col--)
         {
-            bestCount = Math.Max(Math.Max(right, diagonal), down);
-            bestSequence = string.Join(", ",
-                Enumerable
-                .Repeat(matrix[row, col], bestCount)
-                .ToArray());
-        }
-    }
-
-    private static int CheckDown(string[,] matrix, int row, int col)
-    {
-        int count = 0;
-        string checkString = matrix[row, col];
-        for (int i = row; i < matrix.GetLength(0); i++)
-        {
-            if (matrix[i, col] == checkString)
-                ++count;
+            if ((matrix[row, col] == matrix[row + 1, col + 1]))
+            {
+                count++;
+            }
             else
-                break;
+            {
+                count = 1;
+            }
+        
+            if (count > maxCount)
+            {
+                maxCount = count;
+            }
         }
-        return count;
+        
+        count = 1;
+
+        Console.WriteLine(maxCount);
     }
-
-    private static int CheckRight(string[,] matrix, int row, int col)
-    {
-        int count = 0;
-        string checkString = matrix[row, col];
-        for (int j = col; j < matrix.GetLength(1); j++)
-        {
-            if (matrix[row, j] == checkString)
-                ++count;
-            else
-                break;
-        }
-        return count;
-    }
-
-    private static int CheckDiagonal(string[,] matrix, int row, int col)
-    {
-        int count = 0;
-        string checkString = matrix[row, col];
-        for (int i = row, j = col; i < matrix.GetLength(0) && j < matrix.GetLength(1); i++, j++)
-        {
-            if (matrix[i, j] == checkString)
-                ++count;
-            else
-                break;
-        }
-
-        return count;
-    }
-
 }
 
