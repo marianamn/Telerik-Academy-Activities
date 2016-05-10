dc99d9cac51d3c13ddca22270501067072f86254
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-10 16:06:20 +0300

    Update EnglishDigit.cs
    Rewrite digits with lower letter

--- Homeworks/03. CSharp-Advance/03. Methods/EnglishDigit/EnglishDigit.cs
+++ Homeworks/03. CSharp-Advance/03. Methods/EnglishDigit/EnglishDigit.cs
@@ -15,16 +15,16 @@ class EnglishDigit
 
         switch (latsDigit)
         {
-            case 1: Console.WriteLine("One"); break;
-            case 2: Console.WriteLine("Two"); break;
-            case 3: Console.WriteLine("Three"); break;
-            case 4: Console.WriteLine("Four"); break;
-            case 5: Console.WriteLine("Five"); break;
-            case 6: Console.WriteLine("Six"); break;
-            case 7: Console.WriteLine("Seven"); break;
-            case 8: Console.WriteLine("Eight"); break;
-            case 9: Console.WriteLine("Nine"); break;
-            default: Console.WriteLine("Zero");
+            case 1: Console.WriteLine("one"); break;
+            case 2: Console.WriteLine("two"); break;
+            case 3: Console.WriteLine("three"); break;
+            case 4: Console.WriteLine("four"); break;
+            case 5: Console.WriteLine("five"); break;
+            case 6: Console.WriteLine("six"); break;
+            case 7: Console.WriteLine("seven"); break;
+            case 8: Console.WriteLine("eight"); break;
+            case 9: Console.WriteLine("nine"); break;
+            default: Console.WriteLine("zero");
                 break;
         }
     }
