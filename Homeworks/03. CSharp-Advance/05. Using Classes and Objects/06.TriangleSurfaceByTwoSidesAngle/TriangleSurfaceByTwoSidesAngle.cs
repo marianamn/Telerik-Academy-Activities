9712c8eb5f08f310e25080efc5d5b6b67d972a49
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-12 11:44:06 +0300

    Update TriangleSurfaceByTwoSidesAngle.cs
    Fix angle to be Parsed as double.

--- Homeworks/03. CSharp-Advance/05. Using Classes and Objects/06.TriangleSurfaceByTwoSidesAngle/TriangleSurfaceByTwoSidesAngle.cs
+++ Homeworks/03. CSharp-Advance/05. Using Classes and Objects/06.TriangleSurfaceByTwoSidesAngle/TriangleSurfaceByTwoSidesAngle.cs
@@ -8,15 +8,15 @@
         {
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
-            double alpha = int.Parse(Console.ReadLine());
+            double angle = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("{0:F2}", SurfaceByTwoSidesAndAngle(a, b, alpha));
+            Console.WriteLine("{0:F2}", SurfaceByTwoSidesAndAngle(a, b, angle));
         }
 
-        static double SurfaceByTwoSidesAndAngle(double a, double b, double alpha)
+        static double SurfaceByTwoSidesAndAngle(double a, double b, double angle)
         {
-            double angle = Convert.ToDouble((Math.PI * alpha) / 180);
-            double surface = Convert.ToDouble((b * a * Math.Sin(angle)) / 2);
+            double radian = (Math.PI * angle) / 180;
+            double surface = (b * a * Math.Sin(radian)) / 2;
             return surface;
         }
     }
