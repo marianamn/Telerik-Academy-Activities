6aec4e69b5f067efa8fd89eac3635bd0d44eea62
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-12 19:24:07 +0300

    Tasks StringInCSharp and CorrectBrackets done.

---
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/01.Strings in CSharp/StringsInCSharp.md
@@ -0,0 +1,16 @@
+##Describe the strings in C#.
+Strings are sequences of characters.Represented by the string data type in C# (System.String)
+
+
+##What is typical for the string data type?
+- String objects contain an immutable (read-only) sequence of character.Strings use Unicode to support multiple languages and alphabets
+- Strings are stored in the dynamic memory (managed heap)
+- String objects are like arrays of characters (char[]).Have fixed length (String.Length).
+- Elements can be accessed directly by index
+
+##Describe the most important methods of the String class
+- string.IndexOf()
+- string.Split()
+- string.Compare()
+- string.Remove()
+- string.Replace()
--- Homeworks/03. CSharp-Advance/06. Strings and Text Processing/03.CorrectBrackets/CorrectBrackets.cs
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/03.CorrectBrackets/CorrectBrackets.cs
@@ -1,15 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _03.CorrectBrackets
+﻿namespace _03.CorrectBrackets
 {
-    class CorrectBrackets
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CorrectBrackets
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            string expression = Console.ReadLine();
+
+            int countOpen = 0;
+            int countClosed = 0;
+            int positionOpen = 0;
+            int positionClosed = 0;
+            bool isCorrect = true;
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    countOpen++;
+                }
+
+                if (expression[i] == ')')
+                {
+                    countClosed++;
+                }
+            }
+
+            positionOpen = expression.IndexOf('(');
+            positionClosed = expression.IndexOf(')');
+
+            if (positionOpen > positionClosed)
+            {
+                isCorrect = false;
+            }
+
+            if (isCorrect && countClosed == countOpen)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorect");
+            }
+
         }
     }
 }
