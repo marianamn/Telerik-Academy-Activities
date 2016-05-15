cad8be162166733d5181f2b922db9f7d44954f8c
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-15 14:20:56 +0300

    ExtractSentences task - done

--- Homeworks/03. CSharp-Advance/06. Strings and Text Processing/08.ExtractSentences/ExtractSentences.cs
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/08.ExtractSentences/ExtractSentences.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _08.ExtractSentences
+﻿namespace _08.ExtractSentences
 {
-    class ExtractSentences
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ExtractSentences
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            string word = Console.ReadLine();
+            string[] text = Console.ReadLine().Split(new[] { '.', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> sentences = new List<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string[] words = text[i].Trim().Split(new char[] { ',', ' ', ':', '-', '(', ')', '/', '*'}, StringSplitOptions.RemoveEmptyEntries);
+
+                if (words.Contains(word))
+                {
+                    sentences.Add(text[i]);
+                    // TODO: if the sentence end is not a dot?
+                    sentences.Add(".");
+                }
+            }
+
+            Console.WriteLine(string.Join("", sentences));
         }
     }
 }
