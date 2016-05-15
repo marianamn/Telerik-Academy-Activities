9938dd3e9f97a5d8777ae8d5f1d8cb89b1b4052e
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-15 14:22:15 +0300

    EncodeDecode task - done

--- Homeworks/03. CSharp-Advance/06. Strings and Text Processing/07.EncodeDecode/EncodeDecode.cs
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/07.EncodeDecode/EncodeDecode.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _07.EncodeDecode
+﻿namespace _07.EncodeDecode
 {
+    using System;
+    using System.Text;
+
     class EncodeDecode
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a string to be encoded: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Enter encryption key: ");
+            string cipher = Console.ReadLine();
+
+            var encodedInput = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                encodedInput.Append((char)(input[i] ^ cipher[i % cipher.Length])); //encoding 
+            }
+
+            Console.WriteLine("Encoded text: {0}", encodedInput);
+
+
+            var decodedText = new StringBuilder();
+            for (int i = 0; i < encodedInput.Length; i++)
+            {
+                decodedText.Append((char)(encodedInput[i] ^ cipher[i % cipher.Length])); //decoding 
+            }
+
+            Console.WriteLine("Decoded text: {0}", decodedText);
         }
     }
 }
