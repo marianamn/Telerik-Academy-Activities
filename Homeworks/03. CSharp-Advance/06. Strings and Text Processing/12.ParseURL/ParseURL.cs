5091fb5cd457f7ab038a038ee5d9ba2ffd319fba
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-15 14:31:41 +0300

    ParseURL task - done

--- Homeworks/03. CSharp-Advance/06. Strings and Text Processing/12.ParseURL/ParseURL.cs
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/12.ParseURL/ParseURL.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _12.ParseURL
+﻿namespace _12.ParseURL
 {
+    using System;
+    using System.Text;
+
     class ParseURL
     {
         static void Main(string[] args)
         {
+            string url = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            string protocol = url.Substring(0, 4);
+            Console.WriteLine("[protocol]={0}", protocol);
+            url = url.Remove(0, protocol.Length + 3);
+            result.Append(url);
+
+            string dotCom = ".com";
+            int indexDotCom = url.IndexOf(dotCom);
+            string server = url.Substring(0, indexDotCom + 4);
+            Console.WriteLine("[server]={0}", server);
+
+            url = url.Remove(0, server.Length);
+            result.Append(url);
+
+            Console.WriteLine("[resource]={0}", url);
         }
     }
 }
