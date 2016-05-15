8cbc15f02333b383cd2c9694f4049b5002703af5
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-15 12:33:01 +0300

    ParseTags task - done

--- Homeworks/03. CSharp-Advance/06. Strings and Text Processing/05.ParseTags/ParseTags.cs
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/05.ParseTags/ParseTags.cs
@@ -1,15 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _05.ParseTags
+﻿namespace _05.ParseTags
 {
-    class ParseTags
+    using System;
+    using System.Text;
+
+    public class ParseTags
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            string text = Console.ReadLine();
+
+            string openTag = "<upcase>";
+            string closeTag = "</upcase>";
+
+            int startIndex = text.IndexOf(openTag);
+            int endIndex = text.IndexOf(closeTag);
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i < text.Length - openTag.Length && text.Substring(i, openTag.Length) == openTag)
+                {
+                    string textToUpper = text.Substring(startIndex + openTag.Length, endIndex - startIndex - openTag.Length).ToUpper();
+                    result.Append(textToUpper);
+
+                    startIndex = text.IndexOf(openTag, startIndex + 1);
+                    endIndex = text.IndexOf(closeTag, endIndex + 1);
+
+                    i += openTag.Length + textToUpper.Length + closeTag.Length;
+                }
+
+                result.Append(text[i]);
+            }
+
+            result = result.Replace(openTag, "");
+            result = result.Replace(closeTag, "");
+
+            Console.WriteLine(result);
         }
     }
 }
