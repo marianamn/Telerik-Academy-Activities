275f950a3c4001f025b29363a4d276200fb4cc09
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-15 14:25:56 +0300

    UnicodeCharacters task - done

--- Homeworks/03. CSharp-Advance/06. Strings and Text Processing/10.UnicodeCharacters/UnicodeCharacters.cs
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/10.UnicodeCharacters/UnicodeCharacters.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _10.UnicodeCharacters
+﻿namespace _10.UnicodeCharacters
 {
+    using System;
+
     class UnicodeCharacters
     {
         static void Main(string[] args)
         {
+            string text = Console.ReadLine();
+
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string eachChar = string.Format("\\u{0:X4}", Convert.ToUInt16(text[i]));
+                Console.Write(eachChar);
+            }
+
+            Console.WriteLine();
         }
     }
 }
