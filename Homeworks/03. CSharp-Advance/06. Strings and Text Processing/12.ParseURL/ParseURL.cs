141c6954296ac70c0f8df831a0b8c4c76640b73b
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-20 19:23:10 +0300

    ParseURL - fix bugs - added IndexOf(":") and added IndexOf("/")

--- Homeworks/03. CSharp-Advance/06. Strings and Text Processing/12.ParseURL/ParseURL.cs
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/12.ParseURL/ParseURL.cs
@@ -11,20 +11,21 @@
 
             StringBuilder result = new StringBuilder();
 
-            string protocol = url.Substring(0, 4);
-            Console.WriteLine("[protocol]={0}", protocol);
+            string protocol = url.Substring(0, url.IndexOf(':'));
+            Console.WriteLine("[protocol] = {0}", protocol);
+
             url = url.Remove(0, protocol.Length + 3);
             result.Append(url);
 
             string dotCom = ".com";
             int indexDotCom = url.IndexOf(dotCom);
-            string server = url.Substring(0, indexDotCom + 4);
-            Console.WriteLine("[server]={0}", server);
+            string server = url.Substring(0, url.IndexOf('/'));
+            Console.WriteLine("[server] = {0}", server);
 
             url = url.Remove(0, server.Length);
             result.Append(url);
 
-            Console.WriteLine("[resource]={0}", url);
+            Console.WriteLine("[resource] = {0}", url);
         }
     }
 }
