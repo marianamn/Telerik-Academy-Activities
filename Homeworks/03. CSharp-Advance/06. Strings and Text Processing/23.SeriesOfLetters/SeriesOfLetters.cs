53249596deee715f67fc1eeb3f8844cbc921c86d
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-20 18:34:16 +0300

    Add SeriesOfLetters task

---
+++ Homeworks/03. CSharp-Advance/06. Strings and Text Processing/23.SeriesOfLetters/SeriesOfLetters.cs
@@ -0,0 +1,28 @@
+﻿namespace _23.SeriesOfLetters
+{
+    using System;
+    using System.Text;
+
+    public class SeriesOfLetters
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i == 0)
+                {
+                    result.Append(text[0]);
+                }
+                else if (text[i - 1] != text[i])
+                {
+                    result.Append(text[i]);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
