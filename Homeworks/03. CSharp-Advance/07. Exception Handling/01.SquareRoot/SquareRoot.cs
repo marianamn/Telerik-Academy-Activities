8d3ab0a91d0c15c677235d0e40c1838991819533
Mariana Naidenova <mariananaidenova@mail.bg>
2016-05-24 10:40:58 +0300

    Create exception handling homework. SquareRoot task - done

---
+++ Homeworks/03. CSharp-Advance/07. Exception Handling/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,34 @@
+﻿namespace _01.SquareRoot
+{
+    using System;
+
+    public class SquareRoot
+    {
+        public static void Main()
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                double sqr = Math.Sqrt(number);
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+                else
+                {
+                    Console.WriteLine("{0:F3}", sqr);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }       
+        }
+    }
+}
