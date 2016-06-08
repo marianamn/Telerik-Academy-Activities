7cb8aedcd24612afe0d73cbdccdc025a38c89142
Mariana Naidenova <mariananaidenova@mail.bg>
2016-06-08 23:56:03 +0300

    add VersionAttribute task

---
+++ Homeworks/05. CSharp-OOP/02. Defining Classes Part 2/04.VersionAttribute/Version.cs
@@ -0,0 +1,26 @@
+﻿namespace _04.VersionAttribute
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Struct |
+                    AttributeTargets.Class |
+                    AttributeTargets.Interface |
+                    AttributeTargets.Enum | 
+                    AttributeTargets.Method, 
+                    AllowMultiple = false)]
+    public class Version : Attribute
+    {
+        private string vers;
+
+        public Version(string version)
+        {
+            this.Vers = version;
+        }
+
+        public string Vers
+        {
+            get { return this.vers; }
+            private set { this.vers = value; }
+        }
+    }
+}
---
+++ Homeworks/05. CSharp-OOP/02. Defining Classes Part 2/04.VersionAttribute/VersionAttributeMain.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.VersionAttribute
+{
+    using System;
+
+    [Version("v. 2.11")]
+    public class VersionAttributeMain
+    {
+        public static void Main()
+        {
+            Type type = typeof(VersionAttributeMain);
+
+            object[] attributes = type.GetCustomAttributes(false);
+
+            foreach (Version item in attributes)
+            {
+                Console.WriteLine(item.Vers);
+            }
+        }
+    }
+}
