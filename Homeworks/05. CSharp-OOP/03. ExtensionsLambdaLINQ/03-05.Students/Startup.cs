d5fc8e17f3e091700fc4726c74e4bef28184a448
Mariana Naidenova <mariananaidenova@mail.bg>
2016-06-19 11:58:58 +0300

    added tasks from 3 to 5

---
+++ Homeworks/05. CSharp-OOP/03. ExtensionsLambdaLINQ/03-05.Students/Startup.cs
@@ -0,0 +1,62 @@
+﻿namespace _03_05.Students
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student("Anton", "Ivanov", 17),
+                new Student("Petar", "Filipov", 20),
+                new Student("Maria", "Nikolova", 25),
+                new Student("Pavel", "Simeonov", 19),
+                new Student("Pavel", "Dimitrov", 23),
+                new Student("Nadia", "Trifonova", 22)
+            };
+
+            Console.WriteLine("Initial lit of students:");
+            PrintStudents(students);
+            Console.WriteLine();
+
+            // task 3
+            Console.WriteLine("Students whose first name is before its last name alphabetically:");
+            var result = students.Where(x => x.FirstName.CompareTo(x.LastName) == -1);
+            PrintStudents(result);
+            Console.WriteLine();
+
+            // task 4
+            Console.WriteLine("Students with age between 18 and 24:");
+            var studentsInGivenAgeRange = students.Where(x => x.Age >= 18 && x.Age >= 24);
+            PrintStudents(studentsInGivenAgeRange);
+            Console.WriteLine();
+
+            // task 5
+            Console.WriteLine("Students sorted in descending order using lambda extension methods:");
+            var sortedStudents = students.OrderByDescending(x => x.FirstName)
+                                         .ThenByDescending(x => x.LastName);
+            PrintStudents(sortedStudents);
+            Console.WriteLine();
+
+            // task 5 - with LINQ
+            Console.WriteLine("Students sorted in descending order using LINQ:");
+            var sortedStudentsWithLINQ = from student in students
+                                          orderby student.FirstName descending,
+                                          student.LastName descending
+                                          select student;
+            PrintStudents(sortedStudents);
+            Console.WriteLine();
+        }
+
+        private static void PrintStudents<T>(IEnumerable<T> students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ Homeworks/05. CSharp-OOP/03. ExtensionsLambdaLINQ/03-05.Students/Student.cs
@@ -0,0 +1,77 @@
+﻿namespace _03_05.Students
+{
+    using System;
+
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Student(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty!");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty!");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private set
+            {
+                if (this.age < 0 || this.age > 150)
+                {
+                    throw new ArgumentException("Age must be between 0 and 150!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}, age:{2}", this.FirstName, this.LastName, this.Age);
+        }
+    }
+}
