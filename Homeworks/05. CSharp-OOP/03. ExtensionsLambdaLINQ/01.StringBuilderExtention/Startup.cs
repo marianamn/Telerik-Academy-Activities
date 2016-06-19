e56499bf0e624693ca29e0c460a1366b3a95296c
Mariana Naidenova <mariananaidenova@mail.bg>
2016-06-19 10:45:11 +0300

    StringBuilderExtention task- done

---
+++ Homeworks/05. CSharp-OOP/03. ExtensionsLambdaLINQ/01.StringBuilderExtention/Startup.cs
@@ -0,0 +1,14 @@
+﻿namespace _01.StringBuilderExtention
+{
+    using System;
+    using System.Text;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            StringBuilder someText = new StringBuilder("Some text for testing.");
+            Console.WriteLine(someText.Substring(0, 10));
+        }
+    }
+}
---
+++ Homeworks/05. CSharp-OOP/03. ExtensionsLambdaLINQ/01.StringBuilderExtention/StringBuilderSubstring.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.StringBuilderExtention
+{
+    using System;
+    using System.Text;
+
+    public static class StringBuilderSubstring
+    {
+        public static StringBuilder Substring(this StringBuilder str, int index, int length)
+        {
+            var result = new StringBuilder(length);
+
+            var text = str.ToString();
+
+            if (index < 0 || index > text.Length - 1)
+            {
+                throw new ArgumentOutOfRangeException("Index must be greater or equal to 0 and less than text length");
+            }
+
+            if (length < 0 || (index + length) > text.Length - 1)
+            {
+                throw new ArgumentOutOfRangeException("Text length should be greater than 0 and index plus substring length must be less than text length");
+            }
+
+            for (int i = index; i < index + length; i++)
+            {
+                result.Append(text[i]);
+            }
+
+            return result;
+        }
+    }
+}
