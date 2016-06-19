789a9978d57b8c857d1b8a85d61637da775c21c9
Mariana Naidenova <mariananaidenova@mail.bg>
2016-06-19 12:10:34 +0300

    Timer task - done

---
+++ Homeworks/05. CSharp-OOP/03. ExtensionsLambdaLINQ/07.Timer/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace _07.Timer
+{
+    using System;
+    using System.Threading;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            //at each 5 seconds on the Console will be written text "tick" 
+            //and this action will be repeated 3 times
+            Timer timer = new Timer(5, 3, delegate () { Console.WriteLine("tick"); });
+            Thread timerThread = new Thread(new ThreadStart(timer.Run));
+            timerThread.Start();
+        }
+    }
+}
