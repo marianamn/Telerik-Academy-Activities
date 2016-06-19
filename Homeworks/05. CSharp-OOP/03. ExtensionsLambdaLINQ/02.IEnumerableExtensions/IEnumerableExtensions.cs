f78343dd24a60721b7ce9bdb7be23b1f8991ffe7
Mariana Naidenova <mariananaidenova@mail.bg>
2016-06-19 10:59:58 +0300

    IEnumerableExtensions task done

---
+++ Homeworks/05. CSharp-OOP/03. ExtensionsLambdaLINQ/02.IEnumerableExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,52 @@
+﻿namespace _02.IEnumerableExtensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class IEnumerableExtensions
+    {
+        public static T SumOfCollection<T>(this IEnumerable<T> collection)
+        {
+            T result = (dynamic)0;
+
+            foreach (var item in collection)
+            {
+                result += (dynamic)item;
+            }
+
+            return result;
+        }
+
+        public static T ProductOfCollection<T>(this IEnumerable<T> collection)
+        {
+            T result = (dynamic)1;
+            foreach (var item in collection)
+            {
+                result *= (dynamic)item;
+            }
+
+            return result;
+        }
+
+        public static T MinOfCollection<T>(this IEnumerable<T> collection)
+        {
+            return collection.Min();
+        }
+
+        public static T MaxOfCollection<T>(this IEnumerable<T> collection)
+        {
+            return collection.Max();
+        }
+
+        public static double AvrgOfCollection<T>(this IEnumerable<T> collection)
+        {
+            T sum = (dynamic)0;
+            foreach (var item in collection)
+            {
+                sum += (dynamic)item;
+            }
+
+            return sum / (dynamic)collection.Count();
+        }
+    }
+}
---
+++ Homeworks/05. CSharp-OOP/03. ExtensionsLambdaLINQ/02.IEnumerableExtensions/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.IEnumerableExtensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Test with int collection:");
+            List<int> testList = new List<int> { 2, 3, 4, 5, 6 };
+            Console.WriteLine("Sum = {0}", IEnumerableExtensions.SumOfCollection(testList));
+            Console.WriteLine("Prod = {0}", IEnumerableExtensions.ProductOfCollection(testList));
+            Console.WriteLine("Min = {0}", IEnumerableExtensions.MinOfCollection(testList));
+            Console.WriteLine("Max = {0}", IEnumerableExtensions.MaxOfCollection(testList));
+            Console.WriteLine("Avrg = {0:F2}", IEnumerableExtensions.AvrgOfCollection(testList));
+
+            Console.WriteLine("Test with double collection:");
+            List<double> testList2 = new List<double> { 2.5, 3.6, 4, 5.8, 6 };
+            Console.WriteLine("Sum = {0}", IEnumerableExtensions.SumOfCollection(testList2));
+            Console.WriteLine("Prod = {0}", IEnumerableExtensions.ProductOfCollection(testList2));
+            Console.WriteLine("Min = {0}", IEnumerableExtensions.MinOfCollection(testList2));
+            Console.WriteLine("Max = {0}", IEnumerableExtensions.MaxOfCollection(testList2));
+            Console.WriteLine("Avrg = {0:F2}", IEnumerableExtensions.AvrgOfCollection(testList2));
+        }
+    }
+}
