c9cec4b362921e3cb0f12173cab6245c879d46e0
Mariana Naidenova <mariananaidenova@mail.bg>
2016-06-24 11:52:14 +0300

    Update 06_QuadraticEquation.js
    correct realroots formula and add if statement  for x1 < x2

--- Homeworks/06. JavaScript-Fundamentals/03. JS Conditional statements/ConditionalStatements - judge system/06_QuadraticEquation.js
+++ Homeworks/06. JavaScript-Fundamentals/03. JS Conditional statements/ConditionalStatements - judge system/06_QuadraticEquation.js
@@ -5,18 +5,21 @@ function solve(args){
         x1,
         x2;
 
-    x1 = (-b - (Math.sqrt(Math.pow(b, 2) - 4 * a * c))) / (2 * a);
-    x2 = (-b + (Math.sqrt(Math.pow(b, 2) - 4 * a * c))) / (2 * a);
+    x1 = (b + (Math.sqrt(Math.pow(b, 2) - 4 * a * c))) / (2 * a);
+    x2 = (b - (Math.sqrt(Math.pow(b, 2) - 4 * a * c))) / (2 * a);
 
-    if ((Math.pow(b, 2) - 4 * a * c) < 0){
+    if ((Math.pow(b, 2) - 4 * a * c) < 0 || a === 0){
         console.log('no real roots');
     }
     else{
-        if ((Math.pow(b, 2) - 4 * a * c) == 0){
+        if ((Math.pow(b, 2) - 4 * a * c) === 0){
             console.log('x1=x2=' + Number(x1).toFixed(2));
         }
-        else{
+        else if(x1 < x2){
             console.log('x1=' + Number(x1).toFixed(2) + '; ' + 'x2=' + Number(x2).toFixed(2));
+        }
+        else{
+            console.log('x1=' + Number(x2).toFixed(2) + '; ' + 'x2=' + Number(x1).toFixed(2));
         }
     }
 }
