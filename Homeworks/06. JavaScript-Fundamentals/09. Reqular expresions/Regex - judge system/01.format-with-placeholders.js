356fd326dbb325adaf477ec133a2648a9544f90c
Mariana Naidenova <mariananaidenova@mail.bg>
2016-07-24 12:45:44 +0300

    Add Regular expressions homework.

---
+++ Homeworks/06. JavaScript-Fundamentals/09. Reqular expresions/Regex - judge system/01.format-with-placeholders.js
@@ -0,0 +1,19 @@
+function solve(args) {
+    var data = JSON.parse(args[0]),
+        result;
+
+    String.prototype.replacePlaceholders = function (data) {
+        let result = this;
+        for (const option in data) {
+            if ({}.hasOwnProperty.call(data, option)) {
+                let regex = new RegExp(`#{${option}}`, 'g');
+                result = result.replace(regex, data[option]);
+            }
+        }
+
+        return result;
+    };
+
+    result = args[1].replacePlaceholders(data);
+    console.log(result);
+}
