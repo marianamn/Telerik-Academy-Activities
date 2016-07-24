b086e408654437ad63c9d1c3a5d15307da383954
Mariana Naidenova <mariananaidenova@mail.bg>
2016-07-24 12:48:31 +0300

    Add DOM Operations and Event model homework

---
+++ Homeworks/08. JavaScript - UI and DOM/01. DOM Operations/adding-elements.js
@@ -0,0 +1,51 @@
+﻿/* 
+Create a function that takes an id or DOM element and an array of contents
+
+* if an id is provided, select the element
+* Add divs to the element
+  * Each div's content must be one of the items from the contents array
+* The function must remove all previous content from the DOM element provided
+* Throws if:
+  * The provided first parameter is neither string or existing DOM element
+  * The provided id does not select anything (there is no element that has such an id)
+  * Any of the function params is missing
+  * Any of the function params is not as described
+  * Any of the contents is neight `string` or `number`
+    * In that case, the content of the element **must not be** changed   
+*/
+
+function task1(element, contents) {
+    var elem,
+        firstChild,
+        fragment,
+        i,
+        len,
+        newDiv,
+        divToAdd;
+
+    if (typeof (element) == 'string') {
+        elem = document.getElementById(element);
+    }
+    else {
+        elem = element;
+    }
+
+    if (!contents || contents.some(function (item) {
+        return (typeof (item) !== 'string' && typeof (item) !== 'number');
+    })) {
+        throw '';
+    }
+
+    newDiv = document.createElement('div');
+    fragment = document.createDocumentFragment();
+
+    for (i = 0, len = contents.length; i < len; i += 1) {
+        divToAdd = newDiv.cloneNode(true);
+        divToAdd.innerHTML = contents[i];
+        fragment.appendChild(divToAdd);
+    }
+
+    elem.appendChild(fragment);
+}
+
+var el = task1('list', ['<div>new div</div>', '<div>new div</div>', '<div>new div</div>', '<div>new div</div>', '<div>new div</div>']);
---
+++ Homeworks/08. JavaScript - UI and DOM/02. Event Model/show-hide.js
@@ -0,0 +1,79 @@
+﻿//Create a function that takes an id or DOM element and:
+//
+//- If an id is provided, select the element
+//- Finds all elements with class button or content within the provided element
+//     - Change the content of all .button elements with "hide"
+//- When a .button is clicked:
+//     - Find the topmost .content element, that is before another .button and:
+//          - If the .content is visible:
+//             > Hide the .content
+//             > Change the content of the .button to "show"
+//          - If the .content is hidden:
+//             > Show the .content
+//             > Change the content of the .button to "hide"
+//          - If there isn't a .content element after the clicked .button and before other .button, do nothing
+//- Throws if:
+//     - The provided DOM element is non-existant
+//     - The id is either not a string or does not select any DOM element
+
+function solve() {
+    return function (selector) {
+        var element,
+            buttons,
+            content,
+            i,
+            len;
+
+        if (typeof (selector) !== 'string' && !(slector instanceof HTMLElement)) {
+           throw new Error("there is no such selector in DOM");
+        }
+
+        element = document.getElementById(selector);
+        if (element == null) {
+            throw new Error("cannot select element with no id provided");
+        }
+
+        buttons = document.getElementsByClassName('button');
+        content = document.getElementsByClassName('content');
+
+        for (i = 0, len = buttons.length; i < len; i += 1) {
+            buttons[i].innerHTML = 'hide';
+            buttons[i].addEventListener('click', function(ev){
+                var clickedButton = ev.target;
+                var nextSibling = clickedButton.nextElementSibling;
+                var firstContent,
+                    validFirstContent = false;
+
+                while(nextSibling){
+                    if(nextSibling.className === 'content'){
+                        firstContent = nextSibling;
+                        nextSibling = nextSibling.nextSibling;
+                        while(nextSibling){
+                            if(nextSibling.className === 'button'){
+                                validFirstContent = true;
+                                break;
+                            }
+                            nextSibling = nextSibling.nextElementSibling;
+                        }
+                        break;
+                    } else {
+                        nextSibling = nextSibling.nextElementSibling;
+                    }
+                }
+
+                if (validFirstContent) {
+                    if (firstContent.style.display === 'none') {
+                        firstContent.style.display = '';
+                        clickedButton.innerHTML = 'hide';
+                    } else {
+                        firstContent.style.display = 'none';
+                        clickedButton.innerHTML = 'show';
+                    }
+                }
+            })
+        }
+    };
+};
+
+var func = solve();
+func('root');
