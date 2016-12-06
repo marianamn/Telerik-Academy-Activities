c72c155ba3b9fd0f83d38d839ee56100916ff097
Mariana Naidenova <mariananaidenova@mail.bg>
2016-12-06 22:22:20 +0200

    Add Searching algorithms homework

---
+++ Homeworks/15. Data strucrures and algorithms/05. Searching algorithms/SearchingAlgorithms/SearchingAlgorithms/ISorter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SearchingAlgorithms
+{
+    public interface ISorter<T> where T : IComparable<T>
+    {
+        void Sort(IList<T> collection);
+    }
+}
---
+++ Homeworks/15. Data strucrures and algorithms/05. Searching algorithms/SearchingAlgorithms/SearchingAlgorithms/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SearchingAlgorithms
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var collection = new SortableCollection<int>(new[] { 22, 11, 101, 33, 0, 101 }); ;
+            Console.WriteLine("Initial collection:");
+            collection.PrintAllItemsOnConsole();
+            Console.WriteLine();
+
+            Console.WriteLine("Linear search 101:");
+            Console.WriteLine(collection.LinearSearch(101));
+            Console.WriteLine();
+
+            Console.WriteLine("Binary search 101:");
+            Console.WriteLine(collection.BinarySearch(101));
+            Console.WriteLine();
+
+            Console.WriteLine("Shuffle:");
+            collection.Shuffle();
+            collection.PrintAllItemsOnConsole();
+            Console.WriteLine();
+
+            Console.WriteLine("Shuffle again:");
+            collection.Shuffle();
+            collection.PrintAllItemsOnConsole();
+        }
+    }
+}
